7f09caf3e98198dac05b34703465958bb595b563
Jean HAKIZ'A <hakizaaddrew@gmail.com>
2022-05-08 12:54:47 +0200

    Removing unused references

--- Lincolle4net/Linqs/WhereIfExtension.cs
+++ Lincolle4net/Linqs/WhereIfExtension.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace System.Linq
+﻿namespace System.Linq
 {
     public static class WhereIfExtension
     {
